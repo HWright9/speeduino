d5181c666d32a745cd28701845ef88ee17de9c77
Pasi Kemppainen <48950874+pazi88@users.noreply.github.com>
2022-12-14 02:17:47 +0200

    Add deceleration enleanment feature (#939)
    * Add decel enleanment feature
    
    * Fix tps and map DOT scaling in TS.
    
    * updates.ino
    
    * Small bug fix.
    
    * One more small bug fix.
    
    * Update cancomms.ino
    
    Cast tps/mapDOT as uint8 to cancomms to retain backwards compatibility
    
    * Fix programmable outputs list.
    
    * Fix programmable outputs in updates.ino
    
    * Add abs around currentStatus.mapDOT

--- speeduino/logger.ino
+++ speeduino/logger.ino
@@ -40,134 +40,135 @@ byte getTSLogEntry(uint16_t byteNum)
     case 19: statusValue = currentStatus.VE1; break; //VE 1 (%)
     case 20: statusValue = currentStatus.VE2; break; //VE 2 (%)
     case 21: statusValue = currentStatus.afrTarget; break;
-    case 22: statusValue = currentStatus.tpsDOT; break; //TPS DOT
-    case 23: statusValue = currentStatus.advance; break;
-    case 24: statusValue = currentStatus.TPS; break; // TPS (0% to 100%)
+    case 22: statusValue = lowByte(currentStatus.tpsDOT); break; //TPS DOT
+    case 23: statusValue = highByte(currentStatus.tpsDOT); break; //TPS DOT
+    case 24: statusValue = currentStatus.advance; break;
+    case 25: statusValue = currentStatus.TPS; break; // TPS (0% to 100%)
     
-    case 25: 
+    case 26: 
       if(currentStatus.loopsPerSecond > 60000) { currentStatus.loopsPerSecond = 60000;}
       statusValue = lowByte(currentStatus.loopsPerSecond); 
       break;
-    case 26: 
+    case 27: 
       if(currentStatus.loopsPerSecond > 60000) { currentStatus.loopsPerSecond = 60000;}
       statusValue = highByte(currentStatus.loopsPerSecond); 
       break;
     
-    case 27: 
+    case 28: 
       currentStatus.freeRAM = freeRam();
       statusValue = lowByte(currentStatus.freeRAM); //(byte)((currentStatus.loopsPerSecond >> 8) & 0xFF);
       break; 
-    case 28: 
+    case 29: 
       currentStatus.freeRAM = freeRam();
       statusValue = highByte(currentStatus.freeRAM); 
       break;
 
-    case 29: statusValue = (byte)(currentStatus.boostTarget >> 1); break; //Divide boost target by 2 to fit in a byte
-    case 30: statusValue = (byte)(currentStatus.boostDuty / 100); break;
-    case 31: statusValue = currentStatus.spark; break; //Spark related bitfield
+    case 30: statusValue = (byte)(currentStatus.boostTarget >> 1); break; //Divide boost target by 2 to fit in a byte
+    case 31: statusValue = (byte)(currentStatus.boostDuty / 100); break;
+    case 32: statusValue = currentStatus.spark; break; //Spark related bitfield
 
     //rpmDOT must be sent as a signed integer
-    case 32: statusValue = lowByte(currentStatus.rpmDOT); break;
-    case 33: statusValue = highByte(currentStatus.rpmDOT); break;
+    case 33: statusValue = lowByte(currentStatus.rpmDOT); break;
+    case 34: statusValue = highByte(currentStatus.rpmDOT); break;
 
-    case 34: statusValue = currentStatus.ethanolPct; break; //Flex sensor value (or 0 if not used)
-    case 35: statusValue = currentStatus.flexCorrection; break; //Flex fuel correction (% above or below 100)
-    case 36: statusValue = currentStatus.flexIgnCorrection; break; //Ignition correction (Increased degrees of advance) for flex fuel
+    case 35: statusValue = currentStatus.ethanolPct; break; //Flex sensor value (or 0 if not used)
+    case 36: statusValue = currentStatus.flexCorrection; break; //Flex fuel correction (% above or below 100)
+    case 37: statusValue = currentStatus.flexIgnCorrection; break; //Ignition correction (Increased degrees of advance) for flex fuel
 
-    case 37: statusValue = currentStatus.idleLoad; break;
-    case 38: statusValue = currentStatus.testOutputs; break;
+    case 38: statusValue = currentStatus.idleLoad; break;
+    case 39: statusValue = currentStatus.testOutputs; break;
 
-    case 39: statusValue = currentStatus.O2_2; break; //O2
-    case 40: statusValue = currentStatus.baro; break; //Barometer value
+    case 40: statusValue = currentStatus.O2_2; break; //O2
+    case 41: statusValue = currentStatus.baro; break; //Barometer value
 
-    case 41: statusValue = lowByte(currentStatus.canin[0]); break;
-    case 42: statusValue = highByte(currentStatus.canin[0]); break;
-    case 43: statusValue = lowByte(currentStatus.canin[1]); break;
-    case 44: statusValue = highByte(currentStatus.canin[1]); break;
-    case 45: statusValue = lowByte(currentStatus.canin[2]); break;
-    case 46: statusValue = highByte(currentStatus.canin[2]); break;
-    case 47: statusValue = lowByte(currentStatus.canin[3]); break;
-    case 48: statusValue = highByte(currentStatus.canin[3]); break;
-    case 49: statusValue = lowByte(currentStatus.canin[4]); break;
-    case 50: statusValue = highByte(currentStatus.canin[4]); break;
-    case 51: statusValue = lowByte(currentStatus.canin[5]); break;
-    case 52: statusValue = highByte(currentStatus.canin[5]); break;
-    case 53: statusValue = lowByte(currentStatus.canin[6]); break;
-    case 54: statusValue = highByte(currentStatus.canin[6]); break;
-    case 55: statusValue = lowByte(currentStatus.canin[7]); break;
-    case 56: statusValue = highByte(currentStatus.canin[7]); break;
-    case 57: statusValue = lowByte(currentStatus.canin[8]); break;
-    case 58: statusValue = highByte(currentStatus.canin[8]); break;
-    case 59: statusValue = lowByte(currentStatus.canin[9]); break;
-    case 60: statusValue = highByte(currentStatus.canin[9]); break;
-    case 61: statusValue = lowByte(currentStatus.canin[10]); break;
-    case 62: statusValue = highByte(currentStatus.canin[10]); break;
-    case 63: statusValue = lowByte(currentStatus.canin[11]); break;
-    case 64: statusValue = highByte(currentStatus.canin[11]); break;
-    case 65: statusValue = lowByte(currentStatus.canin[12]); break;
-    case 66: statusValue = highByte(currentStatus.canin[12]); break;
-    case 67: statusValue = lowByte(currentStatus.canin[13]); break;
-    case 68: statusValue = highByte(currentStatus.canin[13]); break;
-    case 69: statusValue = lowByte(currentStatus.canin[14]); break;
-    case 70: statusValue = highByte(currentStatus.canin[14]); break;
-    case 71: statusValue = lowByte(currentStatus.canin[15]); break;
-    case 72: statusValue = highByte(currentStatus.canin[15]); break;
+    case 42: statusValue = lowByte(currentStatus.canin[0]); break;
+    case 43: statusValue = highByte(currentStatus.canin[0]); break;
+    case 44: statusValue = lowByte(currentStatus.canin[1]); break;
+    case 45: statusValue = highByte(currentStatus.canin[1]); break;
+    case 46: statusValue = lowByte(currentStatus.canin[2]); break;
+    case 47: statusValue = highByte(currentStatus.canin[2]); break;
+    case 48: statusValue = lowByte(currentStatus.canin[3]); break;
+    case 49: statusValue = highByte(currentStatus.canin[3]); break;
+    case 50: statusValue = lowByte(currentStatus.canin[4]); break;
+    case 51: statusValue = highByte(currentStatus.canin[4]); break;
+    case 52: statusValue = lowByte(currentStatus.canin[5]); break;
+    case 53: statusValue = highByte(currentStatus.canin[5]); break;
+    case 54: statusValue = lowByte(currentStatus.canin[6]); break;
+    case 55: statusValue = highByte(currentStatus.canin[6]); break;
+    case 56: statusValue = lowByte(currentStatus.canin[7]); break;
+    case 57: statusValue = highByte(currentStatus.canin[7]); break;
+    case 58: statusValue = lowByte(currentStatus.canin[8]); break;
+    case 59: statusValue = highByte(currentStatus.canin[8]); break;
+    case 60: statusValue = lowByte(currentStatus.canin[9]); break;
+    case 61: statusValue = highByte(currentStatus.canin[9]); break;
+    case 62: statusValue = lowByte(currentStatus.canin[10]); break;
+    case 63: statusValue = highByte(currentStatus.canin[10]); break;
+    case 64: statusValue = lowByte(currentStatus.canin[11]); break;
+    case 65: statusValue = highByte(currentStatus.canin[11]); break;
+    case 66: statusValue = lowByte(currentStatus.canin[12]); break;
+    case 67: statusValue = highByte(currentStatus.canin[12]); break;
+    case 68: statusValue = lowByte(currentStatus.canin[13]); break;
+    case 69: statusValue = highByte(currentStatus.canin[13]); break;
+    case 70: statusValue = lowByte(currentStatus.canin[14]); break;
+    case 71: statusValue = highByte(currentStatus.canin[14]); break;
+    case 72: statusValue = lowByte(currentStatus.canin[15]); break;
+    case 73: statusValue = highByte(currentStatus.canin[15]); break;
 
-    case 73: statusValue = currentStatus.tpsADC; break;
-    case 74: statusValue = getNextError(); break;
+    case 74: statusValue = currentStatus.tpsADC; break;
+    case 75: statusValue = getNextError(); break;
 
-    case 75: statusValue = lowByte(currentStatus.PW1); break; //Pulsewidth 1 multiplied by 10 in ms. Have to convert from uS to mS.
-    case 76: statusValue = highByte(currentStatus.PW1); break; //Pulsewidth 1 multiplied by 10 in ms. Have to convert from uS to mS.
-    case 77: statusValue = lowByte(currentStatus.PW2); break; //Pulsewidth 2 multiplied by 10 in ms. Have to convert from uS to mS.
-    case 78: statusValue = highByte(currentStatus.PW2); break; //Pulsewidth 2 multiplied by 10 in ms. Have to convert from uS to mS.
-    case 79: statusValue = lowByte(currentStatus.PW3); break; //Pulsewidth 3 multiplied by 10 in ms. Have to convert from uS to mS.
-    case 80: statusValue = highByte(currentStatus.PW3); break; //Pulsewidth 3 multiplied by 10 in ms. Have to convert from uS to mS.
-    case 81: statusValue = lowByte(currentStatus.PW4); break; //Pulsewidth 4 multiplied by 10 in ms. Have to convert from uS to mS.
-    case 82: statusValue = highByte(currentStatus.PW4); break; //Pulsewidth 4 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 76: statusValue = lowByte(currentStatus.PW1); break; //Pulsewidth 1 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 77: statusValue = highByte(currentStatus.PW1); break; //Pulsewidth 1 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 78: statusValue = lowByte(currentStatus.PW2); break; //Pulsewidth 2 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 79: statusValue = highByte(currentStatus.PW2); break; //Pulsewidth 2 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 80: statusValue = lowByte(currentStatus.PW3); break; //Pulsewidth 3 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 81: statusValue = highByte(currentStatus.PW3); break; //Pulsewidth 3 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 82: statusValue = lowByte(currentStatus.PW4); break; //Pulsewidth 4 multiplied by 10 in ms. Have to convert from uS to mS.
+    case 83: statusValue = highByte(currentStatus.PW4); break; //Pulsewidth 4 multiplied by 10 in ms. Have to convert from uS to mS.
 
-    case 83: statusValue = currentStatus.status3; break;
-    case 84: statusValue = currentStatus.engineProtectStatus; break;
-    case 85: statusValue = lowByte(currentStatus.fuelLoad); break;
-    case 86: statusValue = highByte(currentStatus.fuelLoad); break;
-    case 87: statusValue = lowByte(currentStatus.ignLoad); break;
-    case 88: statusValue = highByte(currentStatus.ignLoad); break;
-    case 89: statusValue = lowByte(currentStatus.dwell); break;
-    case 90: statusValue = highByte(currentStatus.dwell); break;
-    case 91: statusValue = currentStatus.CLIdleTarget; break;
-    case 92: statusValue = currentStatus.mapDOT; break;
-    case 93: statusValue = lowByte(currentStatus.vvt1Angle); break; //2 bytes for vvt1Angle
-    case 94: statusValue = highByte(currentStatus.vvt1Angle); break;
-    case 95: statusValue = currentStatus.vvt1TargetAngle; break;
-    case 96: statusValue = (byte)(currentStatus.vvt1Duty); break;
-    case 97: statusValue = lowByte(currentStatus.flexBoostCorrection); break;
-    case 98: statusValue = highByte(currentStatus.flexBoostCorrection); break;
-    case 99: statusValue = currentStatus.baroCorrection; break;
-    case 100: statusValue = currentStatus.VE; break; //Current VE (%). Can be equal to VE1 or VE2 or a calculated value from both of them
-    case 101: statusValue = currentStatus.ASEValue; break; //Current ASE (%)
-    case 102: statusValue = lowByte(currentStatus.vss); break;
-    case 103: statusValue = highByte(currentStatus.vss); break;
-    case 104: statusValue = currentStatus.gear; break;
-    case 105: statusValue = lowByte(currentStatus.fuelPressure); break; //2 bytes for fuelPressure
-    case 106: statusValue = highByte(currentStatus.fuelPressure); break;
-    case 107: statusValue = currentStatus.oilPressure; break;
-    case 108: statusValue = currentStatus.status4; break;
-    case 109: statusValue = lowByte(currentStatus.vvt2Angle); break; //2 bytes for vvt2Angle
-    case 110: statusValue = highByte(currentStatus.vvt2Angle); break;
-    case 111: statusValue = currentStatus.vvt2TargetAngle; break;
-    case 112: statusValue = (byte)(currentStatus.vvt2Duty); break;
-    case 113: statusValue = currentStatus.outputsStatus; break;
-    case 114: statusValue = (byte)(currentStatus.fuelTemp + CALIBRATION_TEMPERATURE_OFFSET); break; //Fuel temperature from flex sensor
-    case 115: statusValue = currentStatus.fuelTempCorrection; break; //Fuel temperature Correction (%)
-    case 116: statusValue = currentStatus.advance1; break; //advance 1 (%)
-    case 117: statusValue = currentStatus.advance2; break; //advance 2 (%)
-    case 118: statusValue = currentStatus.TS_SD_Status; break; //SD card status
-    case 119: statusValue = lowByte(currentStatus.EMAP); break; //2 bytes for EMAP
-    case 120: statusValue = highByte(currentStatus.EMAP); break;
-    case 121: statusValue = currentStatus.fanDuty; break;
-    case 122: statusValue = currentStatus.ego2Correction; break; //Exhaust gas correction bank 2(%)
-    case 123: statusValue = lowByte(currentStatus.InjectorDeltaPress); break; //Injector Delta Pressure
-    case 124: statusValue = highByte(currentStatus.InjectorDeltaPress); break;
-    case 125: statusValue = currentStatus.injPressCorrection; break; //Injector Differential Correction (%)
+    case 84: statusValue = currentStatus.status3; break;
+    case 85: statusValue = currentStatus.engineProtectStatus; break;
+    case 86: statusValue = lowByte(currentStatus.fuelLoad); break;
+    case 87: statusValue = highByte(currentStatus.fuelLoad); break;
+    case 88: statusValue = lowByte(currentStatus.ignLoad); break;
+    case 89: statusValue = highByte(currentStatus.ignLoad); break;
+    case 90: statusValue = lowByte(currentStatus.dwell); break;
+    case 91: statusValue = highByte(currentStatus.dwell); break;
+    case 92: statusValue = currentStatus.CLIdleTarget; break;
+    case 93: statusValue = currentStatus.mapDOT; break;
+    case 94: statusValue = lowByte(currentStatus.vvt1Angle); break; //2 bytes for vvt1Angle
+    case 95: statusValue = highByte(currentStatus.vvt1Angle); break;
+    case 96: statusValue = currentStatus.vvt1TargetAngle; break;
+    case 97: statusValue = (byte)(currentStatus.vvt1Duty); break;
+    case 98: statusValue = lowByte(currentStatus.flexBoostCorrection); break;
+    case 99: statusValue = highByte(currentStatus.flexBoostCorrection); break;
+    case 100: statusValue = currentStatus.baroCorrection; break;
+    case 101: statusValue = currentStatus.VE; break; //Current VE (%). Can be equal to VE1 or VE2 or a calculated value from both of them
+    case 102: statusValue = currentStatus.ASEValue; break; //Current ASE (%)
+    case 103: statusValue = lowByte(currentStatus.vss); break;
+    case 104: statusValue = highByte(currentStatus.vss); break;
+    case 105: statusValue = currentStatus.gear; break;
+    case 106: statusValue = lowByte(currentStatus.fuelPressure); break; //2 bytes for fuelPressure
+    case 107: statusValue = highByte(currentStatus.fuelPressure); break;
+    case 108: statusValue = currentStatus.oilPressure; break;
+    case 109: statusValue = currentStatus.status4; break;
+    case 110: statusValue = lowByte(currentStatus.vvt2Angle); break; //2 bytes for vvt2Angle
+    case 111: statusValue = highByte(currentStatus.vvt2Angle); break;
+    case 112: statusValue = currentStatus.vvt2TargetAngle; break;
+    case 113: statusValue = (byte)(currentStatus.vvt2Duty); break;
+    case 114: statusValue = currentStatus.outputsStatus; break;
+    case 115: statusValue = (byte)(currentStatus.fuelTemp + CALIBRATION_TEMPERATURE_OFFSET); break; //Fuel temperature from flex sensor
+    case 116: statusValue = currentStatus.fuelTempCorrection; break; //Fuel temperature Correction (%)
+    case 117: statusValue = currentStatus.advance1; break; //advance 1 (%)
+    case 118: statusValue = currentStatus.advance2; break; //advance 2 (%)
+    case 119: statusValue = currentStatus.TS_SD_Status; break; //SD card status
+    case 120: statusValue = lowByte(currentStatus.EMAP); break; //2 bytes for EMAP
+    case 121: statusValue = highByte(currentStatus.EMAP); break;
+    case 122: statusValue = currentStatus.fanDuty; break;
+    case 123: statusValue = currentStatus.ego2Correction; break; //Exhaust gas correction bank 2(%)
+    case 124: statusValue = lowByte(currentStatus.InjectorDeltaPress); break; //Injector Delta Pressure
+    case 125: statusValue = highByte(currentStatus.InjectorDeltaPress); break;
+    case 126: statusValue = currentStatus.injPressCorrection; break; //Injector Differential Correction (%)
   }
 
   return statusValue;
